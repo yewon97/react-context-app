1c67d257beee151d0d8855b6228c56495d074ced
yewon97 <dpdnjs9936@gmail.com>
2024-01-06 23:17:13 +0900

    summary 페이지 간단하게 구성하기

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SummaryPage from "./pages/summary";
+
+function App() {
+  return (
+    <div>
+      <SummaryPage />
+    </div>
+  );
+}
+
+export default App;
--- src/pages/summary/index.jsx
+++ src/pages/summary/index.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SummaryPage() {
-  return <div>Summary page!</div>;
+  const [checked, setChecked] = useState(false);
+
+  const onChangeChecked = () => {
+    setChecked((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <form action="">
+        <input
+          type="checkbox"
+          name=""
+          id="confirm-checkbox"
+          checked={checked}
+          onChange={onChangeChecked}
+        />{" "}
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button type="submit" disabled={!checked}>
+          확인
+        </button>
+      </form>
+    </div>
+  );
 }
